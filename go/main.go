1128ea2bb9a4270f6dd2c7f8a7bfeddbd60fe146
Florian Roth <florian.roth@nextron-systems.com>
2020-09-25 12:28:39 +0200

    fix: wrong method in welcome println

--- go/main.go
+++ go/main.go
@@ -69,15 +69,15 @@ func buildHttpTransport(config Config) *http.Transport {
 
 func main() {
 
-	fmt.println(`   ________                __            __                `)
-	fmt.println(`  /_  __/ /  __ _____  ___/ /__ _______ / /____  ______ _  `)
-	fmt.println(`   / / / _ \/ // / _ \/ _  / -_) __(_-</ __/ _ \/ __/  ' \ `)
-	fmt.println(`  /_/ /_//_/\_,_/_//_/\_,_/\__/_/ /___/\__/\___/_/ /_/_/_/ `)
-	fmt.println(`    _____     ____        __                               `)
-	fmt.println(`   / ___/__  / / /__ ____/ /____  ____                     `)
-	fmt.println(`  / /__/ _ \/ / / -_) __/ __/ _ \/ __/                     `)
-	fmt.println(`  \___/\___/_/_/\__/\__/\__/\___/_/                        `)
-	fmt.println(`                                                           `)
+	fmt.Println(`   ________                __            __                `)
+	fmt.Println(`  /_  __/ /  __ _____  ___/ /__ _______ / /____  ______ _  `)
+	fmt.Println(`   / / / _ \/ // / _ \/ _  / -_) __(_-</ __/ _ \/ __/  ' \ `)
+	fmt.Println(`  /_/ /_//_/\_,_/_//_/\_,_/\__/_/ /___/\__/\___/_/ /_/_/_/ `)
+	fmt.Println(`    _____     ____        __                               `)
+	fmt.Println(`   / ___/__  / / /__ ____/ /____  ____                     `)
+	fmt.Println(`  / /__/ _ \/ / / -_) __/ __/ _ \/ __/                     `)
+	fmt.Println(`  \___/\___/_/_/\__/\__/\__/\___/_/                        `)
+	fmt.Println(`                                                           `)
 	fmt.Println(`  Copyright by Nextron Systems GmbH, 2020                  `)
 	fmt.Println(`                                                           `)
 
